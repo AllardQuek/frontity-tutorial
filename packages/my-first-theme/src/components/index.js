45c762d1b20260d4d01767d5a18957198beaf953
Allard <AllardQuek@users.noreply.github.com>
2021-05-17 21:37:45 +0800

    3.3

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -2,12 +2,34 @@
 
 import React from "react"
 import { connect } from "frontity" 
+import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
+import List from "./list"
+import Post from "./post"
+
 
 const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);    // Get the data from whichever route we go
+
   return (
     <>
       <h1>Hello Frontity</h1>
       <p>Current URL: {state.router.link}</p>
+      <nav>
+          <Link link="/">Home</Link>
+          <br />
+          <Link link="/page/2">More posts</Link>
+          <br />
+          <Link link="/about-us">About us</Link>
+      </nav>
+      <hr />
+      <main>
+          <Switch>
+              <List when={data.isArchive} />
+              <Post when={data.isPost}>This is a post</Post>
+              <Post when={data.isPage}>This is a page</Post>
+          </Switch>
+      </main>
     </>
   )
 }
---
+++ packages/my-first-theme/src/components/post.js
@@ -0,0 +1,18 @@
+// File: /packages/my-first-theme/src/components/post.js
+
+import React from "react"
+import { connect } from "frontity"
+
+const Post = ({ state }) => {
+    const data = state.source.get(state.router.link)
+    const post = state.source[data.type][data.id]
+
+    return (
+        <div>
+            <h2>{post.title.rendered}</h2>
+            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>        
+        </div>
+    )
+}
+
+export default connect(Post)
